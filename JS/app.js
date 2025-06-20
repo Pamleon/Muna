6f795a00a3ad19c787d4b7041e095c21c5900b12
Pamela <pamtolaba@hotmail.com>
2025-06-20 13:28:20 +0200

    Subida completa

---
+++ JS/app.js
@@ -0,0 +1,11 @@
+
+
+let currentIndex = 0;
+
+function moveSlide(direction) {
+  const track = document.getElementById("carousel-track");
+  const images = track.querySelectorAll("img");
+  const total = images.length-2; // si agrego mas imagenes debo colocar -1 sino pararece la ultima imagen
+  currentIndex = (currentIndex + direction + total) % total;
+  track.style.transform = `translateX(-${500 * currentIndex}px)`;
+}
