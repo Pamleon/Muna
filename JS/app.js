e78a1de42376f1d89e83a33cd565c45ab408aaa7
Pamela <pamtolaba@hotmail.com>
2025-06-26 01:56:14 +0200

    Pages contacto y productos

--- JS/app.js
+++ JS/app.js
@@ -9,25 +9,5 @@ function moveSlide(direction) {
   currentIndex = (currentIndex + direction + total) % total;
   track.style.transform = `translateX(-${500 * currentIndex}px)`;
 }
-/*Espera a que el DOM cargue completamente*/
-document.addEventListener("DOMContentLoaded", function () {
-  const btnQuienes = document.querySelector('button[data-page="quienes"]');
-  if (btnQuienes) {
-    const seccionNosotros = document.getElementById("nosotros");
-    btnQuienes.addEventListener("click", function () {
-      seccionNosotros.scrollIntoView({ behavior: "smooth" });
-    });
-  }
-  const btnProductos = document.querySelector('button[data-page="productos"]');
-  btnProductos.addEventListener("click", function () {
-    const seccionProductos = document.getElementById("productos");
-    seccionProductos.scrollIntoView({ behavior: "smooth" });
-  });
-  const botones = document.querySelectorAll(".ver-detalles");
-  botones.forEach(boton => {
-    boton.addEventListener("click", function () {
-      const detalles = this.nextElementSibling;
-      detalles.style.display = detalles.style.display === "none" ? "block" : "none";
-    });
-  });
-});
+
+
